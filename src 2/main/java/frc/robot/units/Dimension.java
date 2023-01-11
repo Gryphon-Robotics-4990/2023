f55fb0a5f1e14f9f1806b58eb72fb1cd71dc47ec
Arjun Parthasarathy <56085610+Dat-Boi-Arjun@users.noreply.github.com>
2023-01-10 16:51:04 -0800

    Hopefully actually fixed code architecture this time

--- src 2/main/java/frc/robot/units/Dimension.java
+++
@@ -1,9 +0,0 @@
-package frc.robot.units;
-
-public enum Dimension {
-    Current,
-    Angle,
-    Mass,
-    Length,
-    Time
-}
--- src 2/main/java/frc/robot/units/Unit.java
+++
@@ -1,9 +0,0 @@
-package frc.robot.units;
-
-import java.util.EnumMap;
-
-public interface Unit {
-    public double to(Unit to);
-    public EnumMap<Dimension, Integer> getDimension();
-    public double getScalar();
-}
